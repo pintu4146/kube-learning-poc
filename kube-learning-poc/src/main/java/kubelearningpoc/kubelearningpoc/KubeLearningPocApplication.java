076f4d265c96f4289fc9dc4189ed854d1828c1dc
pintu4146 <pintoo.star4146@gmail.com>
2024-02-27 13:23:04 +0530

    first commit

---
+++ kube-learning-poc/src/main/java/kubelearningpoc/kubelearningpoc/KubeLearningPocApplication.java
@@ -0,0 +1,13 @@
+package kubelearningpoc.kubelearningpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubeLearningPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubeLearningPocApplication.class, args);
+	}
+
+}
